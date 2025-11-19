0a3238a28127f8b65cc605fc4d04f6399dca0c61
Weston Steimel <author@code.w.steimel.me.uk>
2025-11-19 08:44:37 +0000

    feat(julia): support gathering JLSEC aliases
    Signed-off-by: Weston Steimel <author@code.w.steimel.me.uk>

---
+++ src/anchore_security_cli/identifiers/providers/julia.py
@@ -0,0 +1,40 @@
+import logging
+import os
+from glob import iglob
+
+import orjson
+
+from anchore_security_cli.identifiers.aliases import Aliases
+from anchore_security_cli.identifiers.providers.provider import ArchiveProvider, ProviderRecord
+
+
+class Julia(ArchiveProvider):
+    def __init__(self):
+        super().__init__(
+            name="Julia Security Advisories",
+            url="https://osv-vulnerabilities.storage.googleapis.com/Julia/all.zip",
+        )
+
+    def _process_fetch(self, content_dir: str) -> list[ProviderRecord]:
+        records = []
+        for file in iglob(os.path.join(content_dir, "JLSEC-*.json")):
+            if not os.path.isfile(file):
+                continue
+
+            logging.trace(f"processing {self.name} data for {file}")
+            with open(file, "rb") as f:
+                data = orjson.loads(f.read())
+
+            record_id = data["id"]
+            aliases = Aliases.from_list([record_id, *data.get("upstream", [])])
+            published = self._parse_date(data.get("published"))
+
+            records.append(
+                ProviderRecord(
+                    id=record_id,
+                    published=published,
+                    aliases=aliases,
+                ),
+            )
+
+        return records
