7dcaa7e3904953d39a3dc4ef2e5640776803c9d4
Weston Steimel <author@code.w.steimel.me.uk>
2025-11-21 15:40:56 +0000

    feat: extra cve ids from grype db
    Signed-off-by: Weston Steimel <author@code.w.steimel.me.uk>

--- src/anchore_security_cli/identifiers/allocator.py
+++ src/anchore_security_cli/identifiers/allocator.py
@@ -55,7 +55,7 @@ class Allocator:
 
         return aliases
 
-    def allocate(self, refresh: bool = True):
+    def allocate(self, refresh: bool = True):  # noqa: C901
         logging.info(f"Start allocating ids using existing security identifier data from {self.data_path}")
 
         if refresh:
@@ -80,7 +80,16 @@ class Allocator:
             aliases = self.providers.aliases_by_cve(r.id)
             lookups = self._process_record(r, aliases)
             already_processed.update(lookups)
-        logging.info("Finish processing Wordfence CVE  allocations")
+        logging.info("Finish processing Wordfence CVE allocations")
+        logging.info("Start processing GrypeDB extra CVE allocations")
+        for r in self.providers.grypedb_extras.records:
+            if r.id in already_processed:
+                continue
+            logging.debug(f"Processing {r.id}")
+            aliases = self.providers.aliases_by_cve(r.id)
+            lookups = self._process_record(r, aliases)
+            already_processed.update(lookups)
+        logging.info("Finish processing GrypeDB extra CVE allocations")
         logging.info("Start processing GitHub Security Advisory allocations")
         for r in self.providers.github.records:
             if r.id in already_processed:
--- src/anchore_security_cli/identifiers/providers/__init__.py
+++ src/anchore_security_cli/identifiers/providers/__init__.py
@@ -11,7 +11,7 @@ from anchore_security_cli.identifiers.providers.debian import Debian
 from anchore_security_cli.identifiers.providers.echo import Echo
 from anchore_security_cli.identifiers.providers.github import GitHub
 from anchore_security_cli.identifiers.providers.go import Go
-from anchore_security_cli.identifiers.providers.grypedb import GrypeDB
+from anchore_security_cli.identifiers.providers.grypedb import GrypeDB, GrypeDBExtraCVEs
 from anchore_security_cli.identifiers.providers.julia import Julia
 from anchore_security_cli.identifiers.providers.mageia import Mageia
 from anchore_security_cli.identifiers.providers.minimos import MinimOS
@@ -58,6 +58,7 @@ class Providers:
     cpan: CPAN
     arch: Arch
     wordfence: Wordfence
+    grypedb_extras: GrypeDBExtraCVEs
 
     def aliases_by_cve(self, cve_id: str) -> list[str]:
         results = {cve_id}
@@ -127,6 +128,7 @@ def fetch_all() -> Providers:
         cpan = executor.submit(CPAN)
         arch = executor.submit(Arch)
         wordfence = executor.submit(Wordfence)
+        grypedb_extras = executor.submit(GrypeDBExtraCVEs)
 
     return Providers(
         cve5=cve5.result(),
@@ -155,4 +157,5 @@ def fetch_all() -> Providers:
         cpan=cpan.result(),
         arch=arch.result(),
         wordfence=wordfence.result(),
+        grypedb_extras=grypedb_extras.result(),
     )
